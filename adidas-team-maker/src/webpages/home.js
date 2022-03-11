0708fcf0d5b51a65b0698cdbf39a663d83d62382
JavierFananasNaya <javierfananasnaya@gmail.com>
2022-03-11 21:58:41 +0100

    created backend file

--- adidas-team-maker/src/webpages/home.js
+++ adidas-team-maker/src/webpages/home.js
@@ -1,36 +1,32 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 import "./home.css";
 
 const Home = (props) => {
-
   const selectTeamHandler = (team) => {
     props.onSelectTeam(team);
   };
 
-  var myInit = {
-    method: "GET",
-    headers: { "X-Auth-Token": process.env.REACT_APP_API_KEY },
-    dataType: "json",
-    cache: "default",
-  };
-  var myRequest = new Request("https://api.football-data.org/v2/teams", myInit);
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [teams, setTeams] = useState([]);
   useEffect(() => {
-    fetch(myRequest)
-      .then((res) => res.json())
-      .then(
-        (data) => {
-          setIsLoaded(true);
-          console.log(data);
-          setTeams(data.teams);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
+    const options = {
+      method: "GET",
+      url: "http://localhost:8000/teams",
+    };
+
+    axios
+      .request(options)
+      .then((response) => {
+        setIsLoaded(true);
+        console.log(response);
+        setTeams(response.data.teams);
+      })
+      .catch((error) => {
+        setIsLoaded(true);
+        setError(error);
+      });
   }, []);
   if (isLoaded && !error) {
     return (
@@ -38,7 +34,7 @@ const Home = (props) => {
         <ul>
           {teams.map((team) => (
             <li key={team.id}>
-              <button  onClick={() => selectTeamHandler(team)} key={team.id}>
+              <button onClick={() => selectTeamHandler(team)} key={team.id}>
                 <div className="team-list-element">
                   <img className="team-logo" src={team.crestUrl} alt="Logo" />
                   <span>{team.name}</span>
